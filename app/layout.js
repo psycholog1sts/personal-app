a75ce1e39dba369ac6f9881cd5f30b4c77352f0c
psycholog1sts <133178154+psycholog1sts@users.noreply.github.com>
2026-09-04 21:16:19 +0300

    refactor: keep homepage metadata out of root layout

--- app/layout.js
+++ app/layout.js
@@ -2,25 +2,16 @@ import './globals.css';
 import './professional.css';
 import './trust.css';
 import { getLocale } from '../i18n/config.js';
-import { getDictionary } from '../i18n/get-dictionary.js';
-import { buildPageMetadata } from '../i18n/seo.js';
 
-const siteUrl = process.env.NEXT_PUBLIC_SITE_URL?.replace(/\/$/, '');
-const copy = getDictionary('en');
 const locale = getLocale('en');
-
-export const metadata = buildPageMetadata({
-  locale: 'en',
-  pathname: '/',
-  title: copy.meta.home.title,
-  description: copy.meta.home.description,
-  siteUrl,
-});
 
 export default function RootLayout({ children }) {
   return (
     <html lang={locale.htmlLang} dir={locale.dir}>
-      <body>{children}</body>
+      <body>
+        <a className="skipLink" href="#main-content">Skip to content</a>
+        {children}
+      </body>
     </html>
   );
 }
